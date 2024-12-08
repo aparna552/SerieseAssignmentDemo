77592a6ce63259be02824b655ed80b809c716afd
Your Name <aparnapanigrahy552@gmail.com>
2024-12-08 19:44:15 +0530

    Add project files.

---
+++ SerieseAssignmentDemo/Addseriese.aspx
@@ -0,0 +1,124 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Addseriese.aspx.cs" Inherits="SerieseAssignmentDemo.Addseriese" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+        <!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"/>
+<!-- Bootstrap JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet"/>
+    <title>Add or Update Series</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="msg" runat="server" ></asp:Label>
+        </div>
+       <div class="container mt-5">
+           
+            <div class="card">
+                <div class="card-header bg-primary text-white text-center">
+                    <h4 id="lblTitle" runat="server">Add or Update Series</h4>
+                </div>
+                <div class="card-body">
+                    <div class="row g-3">
+                       
+                        <div class="col-md-4">
+                            <label for="txtSeriesName" class="form-label">Series Name:</label>
+                            <asp:TextBox ID="txtSeriesName" runat="server" CssClass="form-control" />
+                        </div>
+                        <div class="col-md-4">
+                            <label for="ddlSeriesType" class="form-label" >Series Type:</label>
+                            <asp:DropDownList ID="ddlSeriesType" runat="server" CssClass="form-select" DataValueField="SeriesType" DataTextField="SeriesType">
+                                <asp:ListItem Text="Select" Value="" />
+                               
+                            </asp:DropDownList>
+                        </div>
+                        <div class="col-md-4">
+                            <label for="ddlMatchType" class="form-label">Match Type:</label>
+                            <asp:DropDownList ID="ddlMatchType" runat="server" CssClass="form-select" DataValueField="MatchType" DataTextField="MatchType">
+                                <asp:ListItem Text="Select" Value="" />
+                                
+                            </asp:DropDownList>
+                        </div>
+                    </div>
+
+                    <div class="row g-3 mt-3">
+                        
+                        <div class="col-md-4">
+                            <label for="ddlGender" class="form-label">Gender:</label>
+                            <asp:DropDownList ID="ddlGender" runat="server" CssClass="form-select" DataValueField="Gender" DataTextField="Gender">
+                                <asp:ListItem Text="Select" Value="" />
+                               
+                            
+                            </asp:DropDownList>
+                        </div>
+                        <div class="col-md-4">
+                            <label for="txtYear" class="form-label">Year:</label>
+                            <asp:TextBox ID="txtYear" runat="server" CssClass="form-control" />
+                        </div>
+                        <div class="col-md-4">
+                            <label for="txtTrophyType" class="form-label">Trophy Type:</label>
+                            <asp:TextBox ID="txtTrophyType" runat="server" CssClass="form-control" />
+                        </div>
+                    </div>
+
+                    <div class="row g-3 mt-3">
+                        
+                        <div class="col-md-6">
+                            <label for="txtStartDate" class="form-label">Series Start Date:</label>
+                            <asp:TextBox ID="txtStartDate" runat="server" CssClass="form-control" TextMode="Date" />
+                        </div>
+                        <div class="col-md-6">
+                            <label for="txtEndDate" class="form-label">Series End Date:</label>
+                            <asp:TextBox ID="txtEndDate" runat="server" CssClass="form-control" TextMode="Date" />
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer text-center">
+                    <asp:Button ID="btnSave" runat="server" CssClass="btn btn-success px-4" Text="Save" OnClick="btnSave_Click"  />
+                 
+                      
+                
+                </div>
+            </div>
+           <div class="card">
+
+           <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+    DataKeyNames="SeriesEID" OnRowEditing="GridView1_RowEditing"
+
+    OnRowUpdating="GridView1_RowUpdating" OnRowDeleting="GridView1_RowDeleting" 
+    OnRowCancelingEdit="GridView1_RowCancelingEdit" 
+    CssClass="table table-striped table-bordered">
+    <Columns>
+      
+        <asp:BoundField DataField="SeriesEID" HeaderText="ID" ReadOnly="True" />
+        
+      
+        <asp:BoundField DataField="SeriesName" HeaderText="Series Name" />
+        <asp:BoundField DataField="SeriesType" HeaderText="Series Type" />
+        <asp:BoundField DataField="MatchType" HeaderText="Match Type" />
+        <asp:BoundField DataField="Gender" HeaderText="Gender" />
+        <asp:BoundField DataField="Year" HeaderText="Year" />
+        <asp:BoundField DataField="TrophyType" HeaderText="Trophy Type" />
+        <asp:BoundField DataField="SeriesStartDate" HeaderText="Start Date" DataFormatString="{0:yyyy-MM-dd}" />
+        <asp:BoundField DataField="SeriesEndDate" HeaderText="End Date" DataFormatString="{0:yyyy-MM-dd}" />
+        
+
+        <asp:CommandField ShowEditButton="True" ShowDeleteButton="True" ButtonType="Button"   />
+    </Columns>
+    <RowStyle BackColor="#EFF3FB" />
+    <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+</asp:GridView>
+
+           </div>
+
+
+        </div>  
+
+    </form>
+</body>
+</html>
---
+++ SerieseAssignmentDemo/Addseriese.aspx.cs
@@ -0,0 +1,202 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+namespace SerieseAssignmentDemo
+{
+    public partial class Addseriese : System.Web.UI.Page
+    {
+
+        string connqu = ConfigurationManager.ConnectionStrings["cons"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                view();
+                bindseriesetype();
+                bindmatchtype();
+                bindGender();
+            }
+
+
+        }
+
+        public void view()
+        {
+            SqlConnection con = new SqlConnection(connqu);
+            con.Open();
+            SqlDataAdapter adp = new SqlDataAdapter("prcTblSeriesView", con);
+            adp.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            con.Close();
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+        public void bindseriesetype()
+        {
+            SqlConnection con = new SqlConnection(connqu);
+            con.Open();
+            string query = "SELECT DISTINCT SeriesType FROM tbl_Series";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+
+
+            ddlSeriesType.DataSource = dt;
+
+            ddlSeriesType.DataBind();
+
+
+            ddlSeriesType.Items.Insert(0, new ListItem("Select", ""));
+
+
+
+
+        }
+
+        public void bindmatchtype()
+        {
+            SqlConnection con = new SqlConnection(connqu);
+            con.Open();
+            string query = "SELECT DISTINCT MatchType FROM tbl_Series";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+
+
+            ddlMatchType.DataSource = dt;
+
+            ddlMatchType.DataBind();
+
+
+            ddlMatchType.Items.Insert(0, new ListItem("Select", ""));
+
+
+
+
+        }
+
+
+        public void bindGender()
+        {
+            SqlConnection con = new SqlConnection(connqu);
+            con.Open();
+            string query = "SELECT DISTINCT Gender FROM tbl_Series";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+
+
+            ddlGender.DataSource = dt;
+
+            ddlGender.DataBind();
+
+
+            ddlGender.Items.Insert(0, new ListItem("Select", ""));
+
+
+
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connqu);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("prcTblSeriesInsert", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@SeriesName", txtSeriesName.Text);
+            cmd.Parameters.AddWithValue("@SeriesType", ddlSeriesType.SelectedValue);
+            cmd.Parameters.AddWithValue("@MatchType", ddlMatchType.SelectedValue);
+            cmd.Parameters.AddWithValue("@Gender", ddlGender.SelectedValue);
+            cmd.Parameters.AddWithValue("@Year", Convert.ToInt32(txtYear.Text));
+            cmd.Parameters.AddWithValue("@TrophyType", txtTrophyType.Text);
+            cmd.Parameters.AddWithValue("@SeriesStartDate", txtStartDate.Text);
+            cmd.Parameters.AddWithValue("@SeriesEndDate", txtEndDate.Text);
+            cmd.ExecuteNonQuery();
+            msg.Text = "Data Saved Successfully";
+            view();
+
+          }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            view();
+
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+
+            string seriesName = ((TextBox)row.Cells[1].Controls[0]).Text;
+            string seriesType = ((TextBox)row.Cells[2].Controls[0]).Text;
+            string matchType = ((TextBox)row.Cells[3].Controls[0]).Text;
+            string gender = ((TextBox)row.Cells[4].Controls[0]).Text;
+            int year = Convert.ToInt32(((TextBox)row.Cells[5].Controls[0]).Text);
+            string trophyType = ((TextBox)row.Cells[6].Controls[0]).Text;
+            DateTime startDate = Convert.ToDateTime(((TextBox)row.Cells[7].Controls[0]).Text);
+            DateTime endDate = Convert.ToDateTime(((TextBox)row.Cells[8].Controls[0]).Text);
+
+
+            SqlConnection con = new SqlConnection(connqu);
+            
+                con.Open();
+                SqlCommand cmd = new SqlCommand("prcTblSeriesUpdate", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@SeriesEID", id);
+                cmd.Parameters.AddWithValue("@SeriesName", seriesName);
+                cmd.Parameters.AddWithValue("@SeriesType", seriesType);
+                cmd.Parameters.AddWithValue("@MatchType", matchType);
+                cmd.Parameters.AddWithValue("@Gender", gender);
+                cmd.Parameters.AddWithValue("@Year", year);
+                cmd.Parameters.AddWithValue("@TrophyType", trophyType);
+                cmd.Parameters.AddWithValue("@SeriesStartDate", startDate);
+                cmd.Parameters.AddWithValue("@SeriesEndDate", endDate);
+
+                cmd.ExecuteNonQuery(); 
+                con.Close();
+            
+        }
+
+
+            protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+
+            SqlConnection con = new SqlConnection(connqu);
+            
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM tbl_Series WHERE SeriesEID=@ID", con);
+                cmd.Parameters.AddWithValue("@ID", id);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            
+
+            view();
+        }
+
+
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1; 
+            view();
+
+        }
+    }
+}
